086f29ba9b6e03c63b4f87d5548078312d1dc594
Chelsi Nolasco <chelsinolasco@MacBook-Pro-2.local>
2019-07-30 09:30:08 +0200

    playing around with snake game

---
+++ games/snake/scripts/setting.js
@@ -0,0 +1,8 @@
+const mycanvas = document.getElementById('gamecanvas');
+const ctx = mycanvas.getContext('2d');
+const w = 350;
+const h = 350;
+let snakeSize = 10;
+let score = 0;
+let snake;
+let food;
