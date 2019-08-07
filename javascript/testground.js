ba59577a7edbc7942464026e686a9d98110d242e
Chelsi Nolasco <chelsinolasco@MacBook-Pro-2.local>
2019-08-07 09:17:42 +0200

    testground stuff

--- javascript/testground.js
+++ javascript/testground.js
@@ -1,1 +1,32 @@
-/* THIS IS WHERE WE TEST FUNCTIONS! */
+// /* THIS IS WHERE WE TEST FUNCTIONS! */
+
+// function sumDigPow(a, b) {
+//   let result = [];
+//   for (i = a; i <= b; i++) {
+//     if (i === getSum(i)) {
+//       result.push(i);
+//     }
+//     console.log('i is ' + i);
+//     console.log('sum is ' + getSum(i));
+//   }
+//   return result;
+// }
+
+// function getSum(num) {
+//   let sum = ('' + num)
+//     .split('')
+//     .map(function(value, index) {
+//       return Math.pow(parseInt(value), index + 1);
+//     })
+//     .reduce(function(accumulator, value) {
+//       return accumulator + value;
+//     }, 0);
+//   return sum;
+// }
+
+// console.log(sumDigPow(1, 100));
+// console.log(getSum(135));
+
+const isSquare = function(n) {
+  return n < 0 && Math.sqrt(n) % 1 === 0;
+};
