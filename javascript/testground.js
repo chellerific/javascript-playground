97082f247b5cb26642084565f089dffc8a0cbab8
Chelsi Nolasco <chelsinolasco@MacBook-Pro-2.local>
2019-11-18 11:28:56 +0100

    chart training

--- javascript/testground.js
+++ javascript/testground.js
@@ -27,6 +27,46 @@
 // console.log(sumDigPow(1, 100));
 // console.log(getSum(135));
 
-const isSquare = function(n) {
-  return n < 0 && Math.sqrt(n) % 1 === 0;
+// const isSquare = function(n) {
+//   return n < 0 && Math.sqrt(n) % 1 === 0;
+// };
+
+// PART 1 - Horizontal chart
+
+// let array = [2, 0, 3, 4];
+// const drawChart = array => {
+//   array.forEach(element => {
+//     if (element > 0) {
+//       let newArr = new Array(element);
+//       for (let i = 0; i < newArr.length; i++) {
+//         newArr[i] = '-';
+//       }
+//       console.log(newArr.join('') + '\n');
+//     } else {
+//       console.log('' + '\n');
+//     }
+//   });
+// };
+
+// drawChart(array);
+
+// PART 2 - Vertical chart
+
+let array = [2, 0, 3, 4];
+const drawChart = array => {
+  let rowsLength = Math.max(...array) - 1;
+  const matrix = [...Array(4).fill(' ')].map(elem => Array(4).fill(' '));
+
+  for (let i = 0; i < array.length; i++) {
+    for (let j = rowsLength; j > rowsLength - array[i]; j--) {
+      matrix[j][i] = '|';
+    }
+  }
+
+  matrix.forEach(elem => {
+    console.log(elem.join(''));
+  });
+  console.log(matrix);
 };
+
+drawChart(array);
