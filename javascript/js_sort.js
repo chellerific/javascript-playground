f6d1897a0a332c3a4639b7edfe626940538c1a2b
Chelsi Nolasco <chelsi.nolasco@Hakans-MacBook-Pro-3.local>
2020-03-27 10:48:18 +0100

    added notes on spread and sort

--- javascript/js_sort.js
+++ javascript/js_sort.js
@@ -1,1 +1,5 @@
 /* JAVASCRIPT SORT THEORY AND USECASES */
+let testArr = [9, 16, 74, 2, 34, 54];
+
+const descending = testArr.sort((a, b) => b - a);
+console.log('Descending ', descending);
---
+++ javascript/js_spread.js
@@ -0,0 +1,35 @@
+/* JAVASCRIPT SPREAD OPERATOR CHEAT SHEET */
+
+// Copying an array
+let names = ['Kim', 'Mari', 'Haneul'];
+let newArr = [...names];
+
+// Concatenating arrays
+let arr1 = ['C', 'H', 'E'];
+let arr2 = ['L', 'S', 'I'];
+let result = [...arr1, ...arr2];
+
+// Merge elements together with an invidividual
+let ffviiArr = ['Cloud', 'Tifa', 'Aerith'];
+let newFFVII = ['Barrett', ...ffviiArr];
+
+// Function calls
+let ffviiiArr = ['Squall', 'Laguna', 'Rinoa'];
+const getNames = (f1, f2, f3) => {
+  console.log(`Names: ${f1}, ${f2} and ${f3}`);
+};
+
+getNames(...names);
+
+// Object literals
+const MariID = {
+  id: 999,
+  name: 'Mari'
+};
+
+const MariBio = {
+  age: '14',
+  island: 'Zolis'
+};
+
+const Mari = { ...MariID, ...MariBio };
